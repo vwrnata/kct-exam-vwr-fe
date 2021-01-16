b1b95ee6230b93471487892dc007e678fe0e67ac
Vojtech_Wrnata <vojtech.wrnata@red-fox.cz>
2021-01-16 07:48:04 +0100

    tlacitko pro pridani do oblibenych

--- src/components/breeds/BreedItem.vue
+++ src/components/breeds/BreedItem.vue
@@ -4,6 +4,7 @@
     <td>{{ name }}</td>
     <td>{{ origin }}</td>
     <td><a :href="image.imgUrl">{{image.imgUrl}}</a></td>
+    <td><base-button @click="addToFavorite(id)">Přidat k oblíbeným</base-button></td>
   </tr>
 </template>
 
@@ -11,5 +12,17 @@
 export default {
   props: ["id", "name", "description", "origin", "image"],
   computed: {},
+  methods: {
+    async addToFavorite(id){
+      console.log('Přidávám ' + id);
+      try {
+        await this.$store.dispatch('favorites/addFavorite', {
+          id,
+        });
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+    }
+  }
 };
 </script>
